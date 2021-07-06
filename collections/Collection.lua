84cc3b14e91242c125f09678ed90fed8fe213836
Chris Massie <chris@massie.scot>
2021-07-06 16:12:10 +0100

    Defined functions for Collection and List.
    Defined functions for Collection and List. Not yet implemented.

--- collections/Collection.lua
+++ collections/Collection.lua
@@ -19,6 +19,225 @@ makePseudoClass("Collection", Collection, __internal.Collection.constructor)
 
 
 
--- Class functions here.
+-- Functions:
+--[[
+    size
+    isEmpty
+    hasAnything
 
--- TO DO: Write
+    contains
+    containsAll
+    containsAny
+    containsOnly
+    containsSameItemsAs
+
+    get
+    getWhere
+    getAlsoIn (intersection)
+    getNotIn
+
+    each
+
+    transformed
+
+    with
+    withAll
+
+    add
+    addAll
+
+    without
+    withoutAll
+    withoutWhere
+
+    remove
+    removeAll
+    removeWhere
+    clear
+
+    toTable
+    toString
+    clone
+]]
+
+
+
+
+---@return number
+function Collection:size()
+    error("Not implemented.")
+    return 0
+end
+
+---@return boolean
+function Collection:isEmpty()
+    error("Not implemented.")
+    return true
+end
+
+---@return boolean
+function Collection:hasAnything()
+    error("Not implemented.")
+    return true
+end
+
+
+---@param item T
+---@return boolean
+function Collection:contains(item)
+    error("Not implemented.")
+    return true
+end
+
+---@param items Collection<T>
+---@return boolean
+function Collection:containsAll(items)
+    error("Not implemented.")
+    return true
+end
+
+---@param items Collection<T>
+---@return boolean
+function Collection:containsAny(items)
+    error("Not implemented.")
+    return true
+end
+
+---@param items Collection<T>
+---@return boolean
+function Collection:containsOnly(items)
+    error("Not implemented.")
+    return true
+end
+
+---@param items Collection<T>
+---@return boolean
+function Collection:containsSameItemsAs(items)
+    error("Not implemented.")
+    return true
+end
+
+
+---@param predicate fun(item: T): boolean
+---@return Collection<T>
+function Collection:getWhere(predicate)
+    error("Not implemented.")
+    return --[[---@type Collection<T>]] nil
+end
+
+---@param other Collection<T>
+---@return Collection<T>
+function Collection:getAlsoIn(other)
+    error("Not implemented.")
+    return --[[---@type Collection<T>]] nil
+end
+
+---@param other Collection<T>
+---@return Collection<T>
+function Collection:getNotIn(other)
+    error("Not implemented.")
+    return --[[---@type Collection<T>]] nil
+end
+
+
+---@return (fun(beingIteratedOver: Collection<T>, previousItem: T): T), Collection<T>, number
+function Collection:each()
+    error("Not implemented.")
+    return --[[---@type (fun(beingIteratedOver: Collection<T>, previousItem: T): T), Collection<T>, number]] nil
+end
+
+
+---@generic NT
+---@param transformer fun(x: T): NT
+---@return Collection<NT>
+function Collection:transformed(transformer)
+    error("Not implemented.")
+    return --[[---@type Collection<NT>]] nil
+end
+
+
+---@param item T
+---@return Collection<T>
+function Collection:with(item)
+    error("Not implemented.")
+    return --[[---@type Collection<T>]] nil
+end
+
+---@param items Collection<T>
+---@return Collection<T>
+function Collection:withAll(items)
+    error("Not implemented.")
+    return --[[---@type Collection<T>]] nil
+end
+
+
+---@param item T
+function Collection:add(item)
+    error("Not implemented.")
+end
+
+---@param items Collection<T>
+function Collection:addAll(items)
+    error("Not implemented.")
+end
+
+
+---@param item T
+---@return Collection<T>
+function Collection:without(item)
+    error("Not implemented.")
+    return --[[---@type Collection<T>]] nil
+end
+
+---@param items Collection<T>
+---@return Collection<T>
+function Collection:withoutAll(items)
+    error("Not implemented.")
+    return --[[---@type Collection<T>]] nil
+end
+
+---@param predicate fun(item: T): boolean
+---@return Collection<T>
+function Collection:withoutWhere(predicate)
+    error("Not implemented.")
+    return --[[---@type Collection<T>]] nil
+end
+
+
+---@param item T
+function Collection:remove(item)
+    error("Not implemented.")
+end
+
+---@param items Collection<T>
+function Collection:removeAll(items)
+    error("Not implemented.")
+end
+
+---@param predicate fun(item: T): boolean
+function Collection:removeWhere(predicate)
+    error("Not implemented.")
+end
+
+function Collection:clear()
+    error("Not implemented.")
+end
+
+
+---@return T[]
+function Collection:toTable()
+    error("Not implemented.")
+    return --[[---@type T[] ]] nil
+end
+
+---return string
+function Collection:toString()
+    error("Not implemented.")
+    return --[[---@type string]] nil
+end
+
+---@return Collection<T>
+function Collection:clone()
+    error("Not implemented.")
+    return --[[---@type Collection<T>]] nil
+end
--- collections/List.lua
+++ collections/List.lua
@@ -70,6 +70,7 @@ makePseudoClass("List", List, __internal.List.constructor)
     getNotIn
 
     each
+    eachWithIndex
     eachInOrder
 
     reverse
@@ -97,12 +98,16 @@ makePseudoClass("List", List, __internal.List.constructor)
     withoutFirst
     withoutLast
     withoutInRange
+    withoutAll
+    withoutWhere
 
     remove
     removeAt
     removeFirst
     removeLast
     removeInRange
+    removeAll
+    removeWhere
     clear
 
     toTable
@@ -110,71 +115,326 @@ makePseudoClass("List", List, __internal.List.constructor)
     clone
 ]]
 
---region number of items
-
----@param this List<T>
-function List.size(this)
-    return #(this.content)
-end
-
----@param this List<T>
-function List.isEmpty(this)
-    return #(this.content) == 0
-end
-
----@param this List<T>
-function List.hasAnything(this)
-    return #(this.content) > 0
-end
-
---endregion
-
---region contains
-
----@param this List<T>
----@param item T
-function List.contains(this, item)
-    for _, v in pairs(this.content) do
-        if item == v then
-            return true
-        end
-    end
-
-    return false
-end
-
---endregion
-
---region get
-
----@param this List<T>
----@param index number
-function List.get(this, index)
-    if index <= 0 then
-        error("Attempts to access a list at a 0 or negative index, which was " .. tostring(index) .. ".")
-    end
-
-    local size = #(this.content)
-
-    if index > size then
-        error("Attempted to access a list of size " .. tostring(size) .. " at index " .. tostring(index) .. ".")
-    end
-
-    return this.content[index]
-end
-
---endregion
-
---region add values
-
----@param this List<T>
----@param itemToAdd T
-function List.add(this, itemToAdd)
-    table.insert(this.content, itemToAdd)
-end
-
---endregion
-
 -- TO DO: Continue writing.
+
+
+
+
+---@return number
+function List:size()
+    error("Not implemented yet")
+    return --[[---@type number]] nil
+end
+
+---@return boolean
+function List:isEmpty()
+    error("Not implemented yet")
+    return --[[---@type boolean]] nil
+end
+
+---@return boolean
+function List:hasAnything()
+    error("Not implemented yet")
+    return --[[---@type boolean]] nil
+end
+
+
+---@return boolean
+function List:contains(item)
+    error("Not implemented yet")
+    return --[[---@type boolean]] nil
+end
+
+---@return boolean
+function List:containsAll(items)
+    error("Not implemented yet")
+    return --[[---@type boolean]] nil
+end
+
+---@return boolean
+function List:containsAny(items)
+    error("Not implemented yet")
+    return --[[---@type boolean]] nil
+end
+
+---@return boolean
+function List:containsOnly(items)
+    error("Not implemented yet")
+    return --[[---@type boolean]] nil
+end
+
+---@return boolean
+function List:containsSameItemsAs(items)
+    error("Not implemented yet")
+    return --[[---@type boolean]] nil
+end
+
+---@param items Collection<T>
+---@return boolean
+function List:containsExactly(items)
+    error("Not implemented yet")
+    return --[[---@type boolean]] nil
+end
+
+---@param items List<T>
+---@return boolean
+function List:containsExactlyInSameOrder(items)
+    error("Not implemented yet")
+    return --[[---@type boolean]] nil
+end
+
+
+---@param index number
+---@return T
+function List:get(index)
+    error("Not implemented yet")
+    return --[[---@type T]] nil
+end
+
+---@return T
+function List:getFirst()
+    error("Not implemented yet")
+    return --[[---@type T]] nil
+end
+
+---@return T
+function List:getLast()
+    error("Not implemented yet")
+    return --[[---@type T]] nil
+end
+
+---@param fromInclusive number
+---@param toExclusive number
+---@return List<T>
+function List:getSublist(fromInclusive, toExclusive)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@return List<T>
+function List:getWhere(predicate)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@return List<T>
+function List:getAlsoIn(other)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@return List<T>
+function List:getNotIn(other)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+
+---@return (fun(beingIteratedOver: List<T>, previousItem: T): T), List<T>, number
+function List:each()
+    error("Not implemented yet")
+    return --[[---@type (fun(beingIteratedOver: List<T>, previousItem: T): T), List<T>, number]] nil
+end
+
+---@return (fun(beingIteratedOver: List<T>, previousIndex: number): number, T), List<T>, number
+function List:eachWithIndex()
+    error("Not implemented yet")
+    return --[[---@type (fun(beingIteratedOver: List<T>, previousIndex: number): number, T), List<T>, number]] nil
+end
+
+---@return (fun(beingIteratedOver: List<T>, previousIndex: number): number, T), List<T>, number
+function List:eachInOrder()
+    error("Not implemented yet")
+    return --[[---@type (fun(beingIteratedOver: List<T>, previousIndex: number): number, T), List<T>, number]] nil
+end
+
+
+function List:reverse()
+    error("Not implemented yet")
+end
+
+function List:shuffle()
+    error("Not implemented yet")
+end
+
+---@param NumberOfPlacesToCycle number
+function List:cycle(NumberOfPlacesToCycle)
+    error("Not implemented yet")
+end
+
+function List:cycleRandomly()
+    error("Not implemented yet")
+end
+
+
+---@return List<T>
+function List:reversed()
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@return List<T>
+function List:shuffled()
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@param NumberOfPlacesToCycle number
+---@return List<T>
+function List:cycled(NumberOfPlacesToCycle)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@return List<T>
+function List:cycledRandomly()
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+
+---@generic NT
+---@return List<NT>
+function List:transformed(transformer)
+    error("Not implemented yet")
+    return --[[---@type List<NT>]] nil
+end
+
+
+---@return List<T>
+function List:with(item)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@return List<T>
+function List:withAll(items)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+
+function List:add(item)
+    error("Not implemented yet")
+end
+
+function List:addAll(items)
+    error("Not implemented yet")
+end
+
+
+---@param index number
+---@param item T
+function List:set(index, item)
+    error("Not implemented yet")
+end
+
+
+---@return List<T>
+function List:without(item)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@param index number
+---@return List<T>
+function List:withoutAt(index)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@param amount number | nil
+---@return List<T>
+function List:withoutFirst(amount)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@param amount number | nil
+---@return List<T>
+function List:withoutLast(amount)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@param fromInclusive number
+---@param toExclusive number
+---@return List<T>
+function List:withoutInRange(fromInclusive, toExclusive)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+
+---@return List<T>
+function List:withoutAll(items)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+---@return List<T>
+function List:withoutWhere(predicate)
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+
+function List:remove(item)
+    error("Not implemented yet")
+end
+
+---@param index number
+function List:removeAt(index)
+    error("Not implemented yet")
+end
+
+---@param amount number | nil
+function List:removeFirst(amount)
+    error("Not implemented yet")
+end
+
+---@param amount number | nil
+function List:removeLast(amount)
+    error("Not implemented yet")
+end
+
+---@param fromInclusive number
+---@param toExclusive number
+function List:removeInRange(fromInclusive, toExclusive)
+    error("Not implemented yet")
+end
+
+function List:removeAll(items)
+    error("Not implemented yet")
+end
+
+function List:removeWhere(predicate)
+    error("Not implemented yet")
+end
+
+function List:clear()
+    error("Not implemented yet")
+end
+
+
+---@return T[]
+function List:toTable()
+    error("Not implemented yet")
+    return --[[---@type T[] ]] nil
+end
+
+---@return string
+function List:toString()
+    error("Not implemented yet")
+    return --[[---@type string]] nil
+end
+
+---@return List<T>
+function List:clone()
+    error("Not implemented yet")
+    return --[[---@type List<T>]] nil
+end
+
+
 
 
